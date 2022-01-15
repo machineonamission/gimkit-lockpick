da4e96efe9b509885b593ef9801fac4978790b16
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-01-14 22:43:09 -0600

    finally fix this

--- bg.js
+++ bg.js
@@ -1,16 +1,18 @@
-// set the icon to be enabled on gimkit.com and disabled everywhere else
-let rule = {
-    conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostSuffix: 'gimkit.com'},
-        })
-    ],
-    actions: [new chrome.declarativeContent.ShowAction()],
-};
-chrome.runtime.onInstalled.addListener(function() {
+function declarcontent() {
+    // set the icon to be enabled on gimkit.com and disabled everywhere else
+    let rule = {
+        conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {hostSuffix: 'gimkit.com'},
+            })
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()],
+    };
     chrome.action.disable();
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([rule]);
     });
-});
+}
+chrome.runtime.onStartup.addListener(declarcontent);
+chrome.runtime.onInstalled.addListener(declarcontent);
 
