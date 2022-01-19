ae54824fd3d90049a340f21b395818c0fa8ccb9d
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-01-19 10:45:16 -0600

    un peux de trollge

--- interface/index.js
+++ interface/index.js
@@ -1,3 +1,10 @@
+// force hrefs to work
+window.addEventListener('click', function (e) {
+    if (e.target.href !== undefined) {
+        chrome.tabs.create({url: e.target.href})
+    }
+})
+
 function askwindow(port, type, params = {}) {
     return new Promise(((resolve) => {
         // proper call and response doesnt work well for passing to the window so we have to create IDs to identify each query
