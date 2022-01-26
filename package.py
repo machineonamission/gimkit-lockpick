e3c01b11aab619dd4d92a03298d5ae7b8d9edd5d
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-01-26 10:23:38 -0600

    packaging

--- package.py
+++ package.py
@@ -1,5 +1,5 @@
 import json
-from zipfile import ZipFile
+import zipfile
 import os
 
 file_ignore = [".gitignore", "package.py"]
@@ -10,8 +10,14 @@ dir_name = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(dir_name, "manifest.json"), "r") as json_file:
     manifest = json.load(json_file)
 zip_file_name = os.path.join(dir_name, f"packages/package-{manifest['version']}.zip")
+
+files_written = 0
+# un: 3263667
+# zip: 1704884
+# bzip: 1693187
+# lzma: 1594970
 # create a ZipFile object
-with ZipFile(zip_file_name, 'w') as zip_obj:
+with zipfile.ZipFile(zip_file_name, 'w', compression=zipfile.ZIP_LZMA) as zip_obj:
     # Iterate over all the files in directory
     for folder_name, subfolders, filenames in os.walk(dir_name):
         folder_allowed = True
@@ -27,3 +33,5 @@ with ZipFile(zip_file_name, 'w') as zip_obj:
                     print(file_path)
                     # Add file to zip
                     zip_obj.write(file_path, os.path.relpath(file_path, dir_name))
+                    files_written += 1
+print(f"\nWrote {files_written} file(s) to {zip_file_name} totaling {os.path.getsize(zip_file_name):,} bytes")
